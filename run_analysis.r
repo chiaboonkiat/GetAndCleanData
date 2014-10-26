3f0969ed55d359321e245e98bd37eba05e496d33
chiaboonkiat <u2me_sg@yahoo.com.sg>
2014-10-26 22:15:20 +0800

    added comments to script

--- run_analysis.r
+++ run_analysis.r
@@ -1,4 +1,17 @@
 
+#----------------------------------------------------------------------
+# Function readData will read the data from files in "test" or "train" folders
+# by:
+# 1. setting the working directory to folderName
+# 2. generating the filenames to read in folderName
+# 3. reading the files from folderName
+# 4. setting working directory to Inertial Signals
+# 5. generating the filenames to read in Inertial Signals
+# 6. reading the files from Inertial Signals
+# 7. combining all the data read using cbind
+# 8. setting working directory back 2 levels up
+# 9. returning the combined data frame
+#----------------------------------------------------------------------
 readData <- function(folderName,featureLabels,activityLabels)
 {
     setwd(folderName)
@@ -14,6 +27,7 @@ readData <- function(folderName,featureLabels,activityLabels)
     filenamesInInertialSignals <- c("body_acc_x_","body_acc_y_","body_acc_z_",
                                "body_gyro_x_","body_gyro_y_","body_gyro_z_",
                                "total_acc_x_","total_acc_y_","total_acc_z_")
+    
     # generate the names of files to read data from
     filenamesInInertialSignals <- lapply(filenamesInInertialSignals,paste,folderName,".txt",sep="")
     
@@ -26,8 +40,6 @@ readData <- function(folderName,featureLabels,activityLabels)
     
     dataFrame  
 }
-setwd("Downloads/UCI HAR Dataset/")
-
 
 featureLabels <- (read.table("features.txt"))[[2]]
 activityLabels<-(read.table("activity_labels.txt"))[[2]]
@@ -41,6 +53,11 @@ testData<-readData("test",featureLabels,activityLabels)
 #merge the test and train data into mergedData
 mergedData <- rbind(trainData,testData)
 
+#----------------------------------------------------------------------
+# Function extractData will extract the columns from mergedData which 
+# matches the value of patternToExtract
+# which is Activity, Subject, std() or mean()
+#----------------------------------------------------------------------
 extractData <- function(mergedData,patternToExtract)
 {
     #retrieve measurements that have only std or mean
@@ -51,6 +68,14 @@ extractData <- function(mergedData,patternToExtract)
 #extract data from Activity, Subject and columns containing std() and mean()
 dataExtracted <- extractData(mergedData,"Activity|Subject|std\\(\\)|mean\\(\\)")
 
+#----------------------------------------------------------------------
+# Function relabelColumnNames will rename the variable names to be more 
+# descriptive 
+# 1. fBodyBody and fBody will be renamed to frequencyBody
+# 2. tBody will be renamed to timeBody
+# 3. tGravity will be renamed to timeGravity
+# 4. () will be removed
+#----------------------------------------------------------------------
 relabelColumnNames <- function(dataExtracted)
 {
     #generate more descriptive variable names
@@ -64,6 +89,10 @@ relabelColumnNames <- function(dataExtracted)
 
 dataRelabeled <- relabelColumnNames(dataExtracted)
 
+#----------------------------------------------------------------------
+# Function generateMean will calculate the average of the measurement 
+# columns for each subject and activity.
+#----------------------------------------------------------------------
 generateMean <- function(dataInput)
 {
     # apply mean to all columns except the Activity and Subject
